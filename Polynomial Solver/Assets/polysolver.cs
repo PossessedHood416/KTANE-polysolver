5c21314142e5f732ec20442ba26bbb8875eb2c67
PossessedHood416 <165974543+PossessedHood416@users.noreply.github.com>
2025-07-30 23:12:53 -0400

    v1.1
    Made logging better.
    Massive overhaul on the code to make it cleaner.
    Almost 150 loc shaved, holy.

--- Polynomial Solver/Assets/polysolver.cs
+++ Polynomial Solver/Assets/polysolver.cs
@@ -12,381 +12,235 @@ using Math = ExMath;
 
 public class polysolver : MonoBehaviour {
 
-   //Bomb sound & info
-   public KMBombInfo Bomb;
-   public KMAudio Audio;
+	public KMBombInfo Bomb;
+	public KMAudio Audio;
 
-   //Buttons and shit   8=numbers   S=sign   R=reset  E=enter
-   public KMSelectable[] Button8;
-   public KMSelectable[] ButtonS;
-   public KMSelectable ButtonR;
-   public KMSelectable ButtonE;
-   public TextMesh DisplayText;
+	public KMSelectable[] Button8;
+	public KMSelectable[] ButtonS;
+	public KMSelectable ButtonR;
+	public KMSelectable ButtonE;
+	public TextMesh DisplayText;
 
-   //vars
-   //8 is a placeholder for no coeffecient
-   int Xvalue;
-   private bool ZenModeActive;
-   int Zen;
-   string[] SolveTxt  = {"UH-HUH","RIGHT!","[COOL]","YIPPEE","[OKAY]","GUDJOB","YESSIR","GOT IT","GAMER!","SOLVED","!!!!!!","RADGE."," YES. ","GREAT!","[EPIC]","NICEON","YOUWIN"};
-   string[] StrikeTxt = {"NUH-UH","WRONG!","[LMAO]","DCOLON","[BRUH]","GITGUD","[NOPE]","FALSE!","STUPID","STRIKE","??????","SADGE.","OH NO.","HUH?!?","GUH!?!","LAME-O","PRESSF"};
-   private int[] Polynomial = {8,8,8,8,8,8};
-   private int[] Submission = {8,8,8,8,8,8};
-   private int Stage;
-   private string Sign;
-   private int Cooldown;
-   static int ModuleIdCounter = 1;
-   int ModuleId;
-   private bool ModuleSolved;
+	int Xvalue;
+	private bool ZenModeActive;
+	int Zen;
+	string[] SolveTxt	= {"UH-HUH","RIGHT!","[COOL]","YIPPEE","[OKAY]","GUDJOB","YESSIR","GOT IT","GAMER!","SOLVED","!!!!!!","RADGE."," YES. ","GREAT!","[EPIC]","NICEON","YOUWIN"};
+	string[] StrikeTxt	= {"NUH-UH","WRONG!","[LMAO]","DCOLON","[BRUH]","GITGUD","[NOPE]","FALSE!","STUPID","STRIKE","??????","SADGE.","OH NO.","HUH?!?","GUH!?!","LAME-O","PRESSF"};
+	private List<int> Polynomial = new List<int>();
+	private List<int> Submission = new List<int>();
+	private int Stage;
+	private string Sign;
+	private int Cooldown;
+	static int ModuleIdCounter = 1;
+	int ModuleId;
+	private bool ModuleSolved = false;
 
+	void Awake () { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
+		ModuleId = ModuleIdCounter++;
+		GetComponent<KMBombModule>().OnActivate += Activate;
+		
+		foreach (KMSelectable Key in Button8) Key.OnInteract += delegate () { Button8Press(Key); return false; };
+		foreach (KMSelectable Key in ButtonS) Key.OnInteract += delegate () { ButtonSPress(Key); return false; };
+		
+		ButtonR.OnInteract += delegate () { ButtonRPress(); return false; };
+		ButtonE.OnInteract += delegate () { ButtonEPress(); return false; };
+	}
 
-   void Awake () { //Avoid doing calculations in here regarding edgework. Just use this for setting up buttons for simplicity.
-      ModuleId = ModuleIdCounter++;
-      GetComponent<KMBombModule>().OnActivate += Activate;
-      
-      foreach (KMSelectable Key in Button8) {
-          Key.OnInteract += delegate () { Button8Press(Key); return false; };
-      }
-      foreach (KMSelectable Key in ButtonS) {
-          Key.OnInteract += delegate () { ButtonSPress(Key); return false; };
-      }
-      
-      ButtonR.OnInteract += delegate () { ButtonRPress(); return false; };
-      
-      ButtonE.OnInteract += delegate () { ButtonEPress(); return false; };
+	void Button8Press (KMSelectable Bun){	
+		Bun.AddInteractionPunch();
+		if(ModuleSolved) return;
 
-   }
-   void Button8Press (KMSelectable Bun){
-      
-      Bun.AddInteractionPunch();
-      
-      if(ModuleSolved){
-         return;
-      }
-      
-      for(int i = 0; i < 8; i++){
-         if(Bun == Button8[i]){
+		int i = 0;
+		for(; i < 8; i++) if(Bun == Button8[i]) break;
 
-            for(int j = 0; j <= 6; j++){
+		if(Submission.Count == Polynomial.Count){
+			//the fuckin fnaf1 Light Not Working sfx :D
+			Audio.PlaySoundAtTransform("Error", Bun.transform);
+			return;
+		}
 
-               if ((j > Stage && j!=8) || j == 8){
-                  //the fuckin fnaf1 Light Not Working sfx :D
-                  Audio.PlaySoundAtTransform("Error", Bun.transform);
-                  break;
-               }
+		Submission.Add(i * (Sign == "+" ? 1 : -1));
+		Audio.PlaySoundAtTransform("ButtonPress" + (Submission.Count).ToString(), Bun.transform);
+		Debug.LogFormat("[Polynomial Solver #{0}] Pressed {1}{2}", ModuleId, Sign, i);
+	}
 
-               if(Submission[j] == 8){
-                  
-                  if(Sign == "+"){
-                     Submission[j] = i;
-                  } else{
-                     Submission[j] = i * -1;
-                  }
-                  
-                  //haha Sst sfx go brrrrr
-                  Audio.PlaySoundAtTransform("ButtonPress" + (j+1).ToString(), Bun.transform);
-                  Debug.LogFormat("[Polynomial Solver #{0}] Pressed {1}{2}", ModuleId, Sign, i);
-                  
-                  j = 8;
-               
-               }
-            }
-         }
-      }
-   }
-   void ButtonSPress (KMSelectable Bun){
+	void ButtonSPress (KMSelectable Bun){
+		Bun.AddInteractionPunch();
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Bun.transform);
 
-      Bun.AddInteractionPunch();
-      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, Bun.transform);
+		if(ModuleSolved) return;
+		Sign = (Bun == ButtonS[0] ? "+" : "-");
+	}
 
-      if(ModuleSolved){
-         return;
-      }
+	void ButtonRPress (){
+		ButtonR.AddInteractionPunch();
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, ButtonR.transform);
+		if(ModuleSolved) return;
 
-      if(Bun == ButtonS[0]){
-         Sign = "+";
-      } else if (Bun == ButtonS[1]){
-         Sign = "-";
-      }
-   }
-   void ButtonRPress (){
+		Submission.Clear();
+		Debug.LogFormat("[Polynomial Solver #{0}] Input reset.", ModuleId);
+	}
 
-      ButtonR.AddInteractionPunch();
-      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, ButtonR.transform);
+	void ButtonEPress (){
+		ButtonE.AddInteractionPunch();
+		Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, ButtonE.transform);
+		if(ModuleSolved) return;
 
-      if(ModuleSolved){
-         return;
-      }
+		Debug.LogFormat("[Polynomial Solver #{0}] Submited: {1}", ModuleId, String.Join(", ", Submission.Select(x => x.ToString()).ToArray()) );
+		Debug.LogFormat("[Polynomial Solver #{0}] Answer: {1}", ModuleId, String.Join(", ", Polynomial.Select(x => x.ToString()).ToArray()) );
 
-      for(int i = 0; i < 6; i++){
-         Submission[i] = 8;
-      }
+		if(!Polynomial.SequenceEqual(Submission)){
+			Debug.LogFormat("[Polynomial Solver #{0}] Wrong!", ModuleId);
+			Strike();
+			Submission.Clear();
+			return;
+		}
 
-      Debug.LogFormat("[Polynomial Solver #{0}] Input reset.", ModuleId);
+		Debug.LogFormat("[Polynomial Solver #{0}] Right!", ModuleId);
+		Audio.PlaySoundAtTransform("InputCorrect", ButtonE.transform);
+		Submission.Clear();	
 
+		if (Stage >= 5) Solve();
+		else {
+			Stage++;
+			Cooldown = (int)Bomb.GetTime()-(2*Zen);
+			DisplayText.text = ("STAGE" + Stage.ToString());
+			MakePoly(Stage);
+		}
+	}
 
-   }
-   void ButtonEPress (){
+	void Activate () { //Shit that should happen when the bomb arrives (factory)/Lights turn on
+		Zen = (ZenModeActive ? -1 : 1);
+		Debug.LogFormat("[Polynomial Solver #{0}] Detected timer ticking {1}.", ModuleId, (ZenModeActive ? "up" : "down"));
 
-      ButtonE.AddInteractionPunch();
-      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, ButtonE.transform);
+		DisplayText.text = "STAGE1";
+		Sign = "+";
+		Stage = 1;
+		Cooldown = (int)Bomb.GetTime()-(2*Zen);
 
-      if(ModuleSolved){
-         return;
-      }
+		MakePoly(Stage);
+	}
 
-      bool Good = true;
+	void Update () {
+		if(ModuleSolved) return;
+		
+		Xvalue = Modulo((int)(Bomb.GetTime()*-1*Zen), (Stage + 2));
 
-      for(int i = 0; i < 6; i++){
-         
-         //how tf do i optimize this into smthn like a dropdown
-         if(Polynomial[i] != 8){
-            if(Submission[i] != 8){   
-               Debug.LogFormat("[Polynomial Solver #{0}] Checking polynomial: {1} vs {2}", ModuleId, Submission[i], Polynomial[i]);
-            } else {
-               Debug.LogFormat("[Polynomial Solver #{0}] Error, too few coefficients were entered, striking...", ModuleId);
-               Good = false;
-               break;
-            }
-         }
+		if(Cooldown*Zen > Bomb.GetTime()*Zen)
+			DisplayText.text = (Xvalue == 0 ? "" : (Calc(Xvalue)).ToString());
 
-         if(Submission[i] != Polynomial[i]){
-            Good = false;
-         }
-      }
+		else if(Cooldown - 5 > Bomb.GetTime())
+			Cooldown = (int)Bomb.GetTime()+2;
 
-      if (Good){
-         Debug.LogFormat("[Polynomial Solver #{0}] Right!", ModuleId);
-         
-         Audio.PlaySoundAtTransform("InputCorrect", ButtonE.transform);
-         
-         if (Stage >= 5){
-            Solve();
-         } else {
-            Stage++;
-            Cooldown = (int)Bomb.GetTime()-(2*Zen);
-            DisplayText.text = ("STAGE" + Stage.ToString());
-            MakePoly(Stage);
-         }
+		else if(Cooldown + 5 < Bomb.GetTime())
+			Cooldown = (int)Bomb.GetTime()-2;
+	}
+	
+	void Solve () {
+		GetComponent<KMBombModule>().HandlePass();
+		ModuleSolved = true;
+		Cooldown = (int)Bomb.GetTime()-(2*Zen);
+		DisplayText.text = SolveTxt[Rnd.Range(0,SolveTxt.Length)];
+	}
 
-      } else {
-         Debug.LogFormat("[Polynomial Solver #{0}] Wrong!", ModuleId);
-         Strike();
-      }
-      for(int i = 0; i < 6; i++){
-         Submission[i] = 8;
-      }
-   }
-   void OnDestroy () { //Shit you need to do when the bomb ends
-      //Eltrick reads this
-   }
-   void Activate () { //Shit that should happen when the bomb arrives (factory)/Lights turn on
-      
-      ModuleSolved = false;
+	void Strike () {
+		GetComponent<KMBombModule>().HandleStrike();
+		Cooldown = (int)Bomb.GetTime()-(2*Zen);
+		DisplayText.text = StrikeTxt[Rnd.Range(0,StrikeTxt.Length)];
+	}
 
-      //zen kinda fucks up the displays bc of how they change w/ the timer. I'm 80% sure this fixes it with more spaghetti code
-      if (!ZenModeActive){
-         Zen = 1;
-         Debug.LogFormat("[Polynomial Solver #{0}] Detected timer ticking down.", ModuleId);
-      } else {
-         Zen = -1;
-         Debug.LogFormat("[Polynomial Solver #{0}] Detected timer ticking up.", ModuleId);
-      }
-      
-      DisplayText.text = "STAGE1";
-      Sign = "+";
-      Stage = 1;
-      
-      //idk how, but if i change this line in anyway whatsoever, the display breaks, but this line might also be a reason the mod breaks on tp training aghghgh
-      Cooldown = (int)Bomb.GetTime()-(2*Zen);
+	void MakePoly (int Stage) {
+		Debug.LogFormat("[Polynomial Solver #{0}] Generating polynomial for stage {1} (degree {1}).", ModuleId, Stage);
+		Polynomial.Clear();
+	
+		for(int i = 0; i<= Stage; i++) Polynomial.Add(Rnd.Range(-7, 8));
+		while(Polynomial[0] == 0) Polynomial[0] = Rnd.Range(-7, 8);
 
-      MakePoly(Stage);
-   }
-   void Start () { //Shit that you calculate, usually a majority if not all of the module
-      //Eltrick also reads this
-   }
-   void Update () { //Shit that happens at any point after initialization
+		Debug.LogFormat("[Polynomial Solver #{0}] Coefficients: {1}", ModuleId, String.Join(", ", Polynomial.Select(x => x.ToString()).ToArray()) );
+	}
 
-      if(!ModuleSolved){
-         
-         //Xvalue loops from 0 to (Stage+1) [inc] via bomb timer like fmzn
-         Xvalue = Modulo((int)(Bomb.GetTime()*-1*Zen), (Stage + 2));
+	int Calc (int x){
+		int Total = 0;
+		for (int i = 0; i<= Stage; i++)	Total += Pow(x, Stage - i) * Polynomial[i];
+		return Total;
+	}
 
-         //if mod strikes/solves, it waits to continue to a different display
-         if(Cooldown*Zen > Bomb.GetTime()*Zen){
-            
-            if (Xvalue == 0){
-               DisplayText.text = "";
-            } else {
-               DisplayText.text = (Calc(Xvalue)).ToString();
-            }
+	int Pow (int x, int y){
+		int Total = 1;
+		for (int i = 0; i < y; i++)	Total *= x;
+		return Total;
+	}
 
-         //hopefully the following code fixes all of my problems :prayge:
-         //this code bumps the cooldown to end closer to bombtime if its too far away
-         } else if(Cooldown - 5 > Bomb.GetTime()){
-            Cooldown = (int)Bomb.GetTime()+2;
-         } else if(Cooldown + 5 < Bomb.GetTime()){
-            Cooldown = (int)Bomb.GetTime()-2;
-         }
-      
-      } else
-      if (Cooldown*Zen > Bomb.GetTime()*Zen){
-         DisplayText.text = "";
-      }
-
-   }
-   void Solve () {
-      GetComponent<KMBombModule>().HandlePass();
-      ModuleSolved = true;
-      Cooldown = (int)Bomb.GetTime()-(2*Zen);
-      DisplayText.text = SolveTxt[Rnd.Range(0,SolveTxt.Length)];
-   }
-   void Strike () {
-      GetComponent<KMBombModule>().HandleStrike();
-      Cooldown = (int)Bomb.GetTime()-(2*Zen);
-      DisplayText.text = StrikeTxt[Rnd.Range(0,StrikeTxt.Length)];
-   }
-   void MakePoly (int Stage) {
-      
-      Debug.LogFormat("[Polynomial Solver #{0}] Generating polynomial for stage {1} (degree {1}). Coefficiants are:", ModuleId, Stage);
-      
-      for(int i = 0; i<= Stage; i++){
-
-         Polynomial[i] = Rnd.Range(-7, 7);
-         
-         //makes sure Leading Coeffeciant is not 0, bc that would lower the degree and make the mod easier, we dont want that to happen :) 
-         while(Polynomial[0] == 0){
-            Polynomial[i] = Rnd.Range(-7, 8);
-         }
-
-         //someone tell me how to optimize this, cuz it prints out the coeffs 1by1
-         Debug.LogFormat("[Polynomial Solver #{0}] {1}", ModuleId, Polynomial[i]);
-
-      }
-
-   }
-   int Calc (int x){
-
-      int Total = 0;
-
-      for (int i = 0; i<= Stage; i++){
-
-         Total += Pow(x, Stage - i) * Polynomial[i];
-
-      }
-
-      return Total;
-
-   }
-   int Pow (int x, int y){
-
-      int Total = 1;
-
-      for (int i = 0; i < y; i++){
-         Total *= x;
-      }
-
-      return Total;
-
-   }
-   int Modulo (int num, int mod){
-      //stupid c# not having good modulo
-      return ((num % mod) + mod) % mod;
-   }
+	int Modulo (int num, int mod){
+		return ((num % mod) + mod) % mod;
+	}
 
 //if youre reading this and the display isn't changing on tp training modes, then blame tp :P
 #pragma warning disable 414
-   private readonly string TwitchHelpMessage = @"Use '!{0} + 0 1 2 3 - 4 5 6 7 E R' to press those buttons. Chain with spaces.";
+	private readonly string TwitchHelpMessage = @"Use '!{0} + 0 1 2 3 - 4 5 6 7 E R' to press those buttons. Chain with spaces.";
 #pragma warning restore 414
-   IEnumerator ProcessTwitchCommand (string Command) {
-        
-        Command = Command.Trim().ToUpper();
-        yield return null;
-        
-        string[] Commands = Command.Split(' ');
 
-        for (int i = 0; i < Commands.Length; i++) {
-            if (Commands[i].Length != 1) {
-               yield return "sendtochaterror Error: Commands can only be 1 character long, chain with spaces.";
-               yield break;
-            }
-            if (!"01234567+-ER".Contains(Commands[i])) {
-               yield return "sendtochaterror Error: Valid buttons are: 0 1 2 3 4 5 6 7 + - E R.";
-               yield break;
-            }
-        
-        }
+	IEnumerator ProcessTwitchCommand (string Command) {
+		Command = Command.Trim().ToUpper();
+		yield return null;
+		
+		string[] Commands = Command.Split(' ');
 
-        for (int i = 0; i < Commands.Length; i++) {
-        
-            if ("01234567".Contains(Commands[i])) {
-               
-               Button8[int.Parse(Commands[i])].OnInteract();
+		for (int i = 0; i < Commands.Length; i++) {
+			if (Commands[i].Length != 1) {
+				yield return "sendtochaterror Error: Commands can only be 1 character long, chain with spaces.";
+				yield break;
+			}
+			if (!"01234567+-ER".Contains(Commands[i])) {
+				yield return "sendtochaterror Error: Valid buttons are: 0 1 2 3 4 5 6 7 + - E R.";
+				yield break;
+			}
+		}
 
-            } else
-            if ("+-".Contains(Commands[i])){
-               
-               if(Commands[i] == "+"){
-                  ButtonS[0].OnInteract();
-               } else 
-               if(Commands[i] == "-"){
-                  ButtonS[1].OnInteract();
-               }
+		for (int i = 0; i < Commands.Length; i++) {
+			if ("01234567".Contains(Commands[i])) {
+				Button8[int.Parse(Commands[i])].OnInteract();
 
-            } else
-            if (Commands[i] == "R"){
+			} else if(Commands[i] == "+") {
+				ButtonS[0].OnInteract();
 
-               ButtonR.OnInteract();
+			} else if(Commands[i] == "-") {
+				ButtonS[1].OnInteract();
+			
+			} else if (Commands[i] == "R"){
+				ButtonR.OnInteract();
 
-            } else
-            if (Commands[i] == "E"){
+			} else if (Commands[i] == "E"){
+				ButtonE.OnInteract();
 
-               ButtonE.OnInteract();
+			}
 
-            }
+			yield return new WaitForSeconds(.2f);
+		}
+	}
 
-            //remember to change these back to smthn like (.2f) or smthn
-            yield return new WaitForSeconds(.2f);
+	IEnumerator TwitchHandleForcedSolve () {
+		yield return null;
+		Debug.LogFormat("[Polynomial Solver #{0}] TP autosolver activated, solving module.", ModuleId);
+		
+		while (!ModuleSolved){
+			ButtonR.OnInteract();
 
-        }
+			while(!Polynomial.SequenceEqual(Submission)){
+				yield return new WaitForSeconds(0.3f);
+				
+				int i = Submission.Count;
 
-   }
-   IEnumerator TwitchHandleForcedSolve () {
-      yield return null;
-
-      Debug.LogFormat("[Polynomial Solver #{0}] TP autosolver activated, solving module.", ModuleId);
-      
-      while (!ModuleSolved)
-      {
-
-
-         ButtonR.OnInteract();
-
-         for(int i = 0; i < 6; i++){
-
-            yield return new WaitForSeconds(0.2f);
-
-            if(Polynomial[i] == 8){
-               break;
-            }
-
-            if(Polynomial[i] >= 0){
-               
-               ButtonS[0].OnInteract();
-               Button8[Polynomial[i]].OnInteract();
-            
-            } else if (Polynomial[i] < 0){
-
-               ButtonS[1].OnInteract();
-               Button8[Polynomial[i]*-1].OnInteract();
-
-            }
-
-         }
-         
-         ButtonE.OnInteract();
-
-      }
-      
-   }
+				if(Polynomial[i] >= 0){
+					ButtonS[0].OnInteract();
+					Button8[Polynomial[i]].OnInteract();
+				} else if (Polynomial[i] < 0){
+					ButtonS[1].OnInteract();
+					Button8[Polynomial[i]*-1].OnInteract();
+				}
+			}
+			
+			ButtonE.OnInteract();
+		}
+	}
 }
